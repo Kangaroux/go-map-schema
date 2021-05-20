2d5f5b882a459ff9e2caa6783fcdae55ac51152e
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-05-19 21:34:49 -0400

    Add basic type checking

--- compare.go
+++ compare.go
@@ -5,15 +5,18 @@ import (
 	"reflect"
 )
 
-type MyStruct struct {
-	S string
-	N int
+type FieldMismatch struct {
+	Field    string
+	Expected string
+	Actual   string
 }
 
 type CompareResults struct {
-	dst           interface{}
-	src           map[string]interface{}
-	MissingFields []string
+	MismatchedFields []FieldMismatch
+	MissingFields    []string
+
+	dst interface{}
+	src map[string]interface{}
 }
 
 func (r *CompareResults) Dst() reflect.Type {
@@ -24,20 +27,48 @@ func (r *CompareResults) String() string {
 	return fmt.Sprintf("CompareResults<missing=%v, dst=%s, src=%v>", r.MissingFields, r.Dst().Name(), r.src)
 }
 
+func valueTypeName(v reflect.Value) string {
+	if !v.IsValid() {
+		return "null"
+	}
+
+	return v.Type().Name()
+}
+
+// CanConvert returns whether value v is convertible to type t.
+func CanConvert(t reflect.Type, v reflect.Value) bool {
+	if v.IsValid() {
+		return v.Type().ConvertibleTo(t)
+	}
+
+	return t.Kind() == reflect.Ptr
+}
+
 func Compare(dst interface{}, src map[string]interface{}) *CompareResults {
 	v := reflect.ValueOf(dst)
 	t := v.Elem().Type()
 	results := &CompareResults{
-		dst:           dst,
-		src:           src,
-		MissingFields: []string{},
+		dst:              dst,
+		src:              src,
+		MismatchedFields: []FieldMismatch{},
+		MissingFields:    []string{},
 	}
 
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
 
-		if srcVal, ok := src[f.Name]; ok {
-			fmt.Printf("payload[%s]: %v\n", f.Name, srcVal)
+		if srcField, ok := src[f.Name]; ok {
+			srcValue := reflect.ValueOf(srcField)
+
+			if !CanConvert(f.Type, srcValue) {
+				mismatch := FieldMismatch{
+					Field:    f.Name,
+					Expected: f.Type.Name(),
+					Actual:   valueTypeName(srcValue),
+				}
+
+				results.MismatchedFields = append(results.MismatchedFields, mismatch)
+			}
 		} else {
 			results.MissingFields = append(results.MissingFields, f.Name)
 		}
--- compare_test.go
+++ compare_test.go
@@ -8,12 +8,88 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type mismatch compare.FieldMismatch
+
 type testStruct struct {
 	Foo string
 	Bar int
 	Baz float64
 }
 
+func toJson(val interface{}) string {
+	out, err := json.Marshal(val)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(out)
+}
+
+// Tests that Compare identifies fields in src that can't be converted
+// to the field in dst, due to a type mismatch (e.g. src:string -> dst:int)
+func TestCompare_MismatchedFields(t *testing.T) {
+	tests := []struct {
+		srcJson  string
+		expected []mismatch
+	}{
+		{
+			srcJson:  `{}`,
+			expected: []mismatch{},
+		},
+		{
+			srcJson:  `{"Foo":"","Bar":0,"Baz":3.14}`,
+			expected: []mismatch{},
+		},
+		{
+			srcJson: `{"Foo":null}`,
+			expected: []mismatch{
+				{
+					Field:    "Foo",
+					Expected: "string",
+					Actual:   "null",
+				},
+			},
+		},
+		{
+			srcJson: `{"Bar":"hi"}`,
+			expected: []mismatch{
+				{
+					Field:    "Bar",
+					Expected: "int",
+					Actual:   "string",
+				},
+			},
+		},
+		{
+			srcJson: `{"Foo":true,"Baz":""}`,
+			expected: []mismatch{
+				{
+					Field:    "Foo",
+					Expected: "string",
+					Actual:   "bool",
+				},
+				{
+					Field:    "Baz",
+					Expected: "float64",
+					Actual:   "string",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		// Unmarshal the json into a map.
+		src := make(map[string]interface{})
+		json.Unmarshal([]byte(test.srcJson), &src)
+
+		r := compare.Compare(&testStruct{}, src)
+		require.JSONEq(t, toJson(r.MismatchedFields), toJson(test.expected))
+	}
+}
+
+// Tests that Compare identifies and returns a list of fields that are in
+// dst but not src.
 func TestCompare_MissingFields(t *testing.T) {
 	tests := []struct {
 		srcJson  string
