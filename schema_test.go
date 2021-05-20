46e22f81e879b11800d979be9fa64cdf3ba0fabc
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-05-20 13:18:02 -0400

    Test TypeNameSimple

--- schema_test.go
+++ schema_test.go
@@ -2,6 +2,7 @@ package schema_test
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	schema "github.com/Kangaroux/go-map-schema"
@@ -490,5 +491,93 @@ func TestCompareMapToStruct_MissingFieldsTags(t *testing.T) {
 
 		r, _ := schema.CompareMapToStruct(&TestStructTags{}, src, nil)
 		require.ElementsMatch(t, test.expected, r.MissingFields, test.srcJson)
+	}
+}
+
+func TestTypeNameSimple(t *testing.T) {
+	tests := []struct {
+		val      interface{}
+		expected string
+	}{
+		// uint types
+		{
+			val:      new(uint),
+			expected: "*uint",
+		},
+		{
+			val:      uint(0),
+			expected: "uint",
+		},
+		{
+			val:      uint8(0),
+			expected: "uint",
+		},
+		{
+			val:      uint16(0),
+			expected: "uint",
+		},
+		{
+			val:      uint32(0),
+			expected: "uint",
+		},
+		{
+			val:      uint64(0),
+			expected: "uint",
+		},
+
+		// int types
+		{
+			val:      new(int),
+			expected: "*int",
+		},
+		{
+			val:      int(0),
+			expected: "int",
+		},
+		{
+			val:      int8(0),
+			expected: "int",
+		},
+		{
+			val:      int16(0),
+			expected: "int",
+		},
+		{
+			val:      int32(0),
+			expected: "int",
+		},
+		{
+			val:      int64(0),
+			expected: "int",
+		},
+
+		// float types
+		{
+			val:      new(float32),
+			expected: "*float",
+		},
+		{
+			val:      float32(0),
+			expected: "float",
+		},
+		{
+			val:      float64(0),
+			expected: "float",
+		},
+
+		// other
+		{
+			val:      "hello",
+			expected: "string",
+		},
+		{
+			val:      &TestStruct{},
+			expected: "*TestStruct",
+		},
+	}
+
+	for _, test := range tests {
+		name := schema.TypeNameSimple(reflect.TypeOf(test.val))
+		require.Equal(t, test.expected, name)
 	}
 }
