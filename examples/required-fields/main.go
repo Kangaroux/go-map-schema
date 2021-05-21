dbb7f5a634c880d0d36a2645bf7bf34745bf84d2
Jessie <2302541+Kangaroux@users.noreply.github.com>
2021-05-21 13:31:04 -0400

    Add a required fields example

---
+++ examples/required-fields/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	schema "github.com/Kangaroux/go-map-schema"
+)
+
+// Person is the model we are using.
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+}
+
+// Response is used to generate a JSON response.
+type Response struct {
+	Errors interface{} `json:"errors,omitempty"`
+	OK     bool        `json:"ok"`
+}
+
+func main() {
+	input := `{
+		"first_name": "Jessie",
+		"age": 26
+	}`
+	m := make(map[string]interface{})
+	p := Person{}
+
+	// Load the JSON into a map.
+	if err := json.Unmarshal([]byte(input), &m); err != nil {
+		panic(err)
+	}
+
+	// Check the types.
+	r, err := schema.CompareMapToStruct(&p, m, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp := &Response{
+		OK: true,
+	}
+
+	// Notify the client of any errors.
+	if len(r.MissingFields) > 0 {
+		m := make(map[string]string)
+
+		for _, f := range r.MissingFields {
+			m[f] = "this field is required"
+		}
+
+		resp.OK = false
+		resp.Errors = m
+	}
+
+	respJson, _ := json.Marshal(resp)
+
+	fmt.Println(string(respJson))
+}
